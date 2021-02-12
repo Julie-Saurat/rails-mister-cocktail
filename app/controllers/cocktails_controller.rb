3bac81c207894626edc1491287facd59c6a4908a
Julie Saurat <julie.saurat.s@gmail.com>
2021-02-12 11:44:48 +0100

    comment all img of cocktails

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -32,6 +32,7 @@ class CocktailsController < ApplicationController
   private
 
   def cocktail_params
-    params.require(:cocktail).permit(:name, :image_url)
+    params.require(:cocktail).permit(:name)
+    # params.require(:cocktail).permit(:name, :image_url)
   end
 end
