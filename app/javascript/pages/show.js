16b38631e01ba690a2b99ba9dda6ed3b68869d19
Julie Saurat <julie.saurat.s@gmail.com>
2021-03-28 19:40:35 +0200

    modal added

---
+++ app/javascript/pages/show.js
@@ -0,0 +1,45 @@
+// // Get the modal
+// const modal = document.getElementById("myModal");
+
+// // Get the button that opens the modal
+// const btn = document.getElementById("myBtn");
+
+// // Get the <span> element that closes the modal
+// const span = document.getElementsByClassName("close")[0];
+
+// // When the user clicks on the button, open the modal
+// btn.onclick = function() {
+//   modal.style.display = "block";
+// }
+
+// // When the user clicks on <span> (x), close the modal
+// span.onclick = function() {
+//   modal.style.display = "none";
+// }
+
+// // When the user clicks anywhere outside of the modal, close it
+// window.onclick = function(event) {
+//   if (event.target == modal) {
+//     modal.style.display = "none";
+//   }
+// }
+
+
+const openModal = () => {
+  const modal = document.getElementById("doseModal");
+  const btn = document.getElementById("modalBtn");
+  btn.addEventListener('click', (event) => {
+    modal.style.display = "block";
+  })
+};
+
+const closeModal = () => {
+  const modal = document.getElementById("doseModal");
+  const span = document.getElementsByClassName("close")[0];
+  span.addEventListener('click', (event) => {
+    modal.style.display = "none";
+  })
+};
+
+export { openModal };
+export { closeModal };
